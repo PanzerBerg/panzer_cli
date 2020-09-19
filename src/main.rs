98d344455d63909e02d274ba76d976dbcf1065bd
Matheus Santos <matheus.san99@gmail.com>
2020-09-19 01:09:07 -0300

    Reading files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io::{BufReader, BufRead};
+use std::path;
+use std::fs::File;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    pattern: String,
+    #[structopt(parse(from_os_str))]
+    path: path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::from_args();
+    let content = File::open(&args.path).expect("Cannot open file");
+    let reader = BufReader::new(content);    
+
+    let mut has_pattern = false;
+
+    for line in reader.lines() {
+        let line = line.unwrap();
+        if line.contains(&args.pattern) {
+            has_pattern = true;
+            println!("{}", line);
+        }
+    }
+
+    if !has_pattern {
+        println!("No pattern {} found in {}", &args.pattern, &args.path.display().to_string());
+    }
 }
