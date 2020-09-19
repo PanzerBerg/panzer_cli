d9dc81541ce8e98ea0593c1129a84ad2f4a822d0
Matheus Santos <matheus.san99@gmail.com>
2020-09-19 03:09:01 -0300

    Created basic structure

---
+++ src/args.rs
@@ -0,0 +1,42 @@
+use clap::ArgMatches;
+
+pub fn help_all() {
+    println!("\n\r      '\
+                \r       _\\______
+                \r      /        \\========
+                \r ____|__________\\_____
+                \r/ ___________________ \
+                \r\\/ _===============_ \\/
+                \r  '-===============-'\n\n
+                \rPanzer CLI help:\n
+                \rARGUMENTS:
+                \rNOTE: Argument values can also be 'all' that returns a list instead of a single value.
+                \r'--p' or '--processes' <VALUE>    'Returns a current running process in the system.'
+                \r'--c' or '--components' <VALUE>   'Returns a hardware component information and temperature.'
+                \n
+                \rNOTE: Arguments can be followed by '-v' that prints the prints a full report of the steps the CLI is taking.\n\n");
+}
+
+pub fn help_with_arg() {
+    println!("\nERROR:
+                \rCan't execute '--help' with multiple arguments.\n
+                \rYou can either call '--help' on a single argument, or without any.
+                \n
+                \r--<ARGUMENT> --help     'Shows the help for that argument'\n
+                \r--help                  'Shows a help for all the arguments available'\n")
+}
+
+pub fn help_verbose(matches: ArgMatches) {
+    println!("Calls a argument module with verbose
+                \rArgs: {:?}", matches.args);
+}
+
+pub fn help_single_arg(matches: ArgMatches) {
+    println!("Help with argument! (Call help inside the arg module)
+            \rArgs: {:?}", matches.args);
+}
+
+pub fn call_arg_module(matches: ArgMatches) {
+    println!("Calls a argument module
+                \rArg: {:?}", matches.args);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,42 @@
-use std::io::{BufReader, BufRead};
-use std::path;
-use std::fs::File;
-use structopt::StructOpt;
-
-#[derive(StructOpt)]
-struct Cli {
-    pattern: String,
-    #[structopt(parse(from_os_str))]
-    path: path::PathBuf,
-}
+use clap::{Arg, App};
+mod args;
 
 fn main() {
-    let args = Cli::from_args();
-    let content = File::open(&args.path).expect("Cannot open file");
-    let reader = BufReader::new(content);    
+    let matches = App::new("Panzer CLI")
+                        .version("0.1.0")
+                        .author("Matheus Santos")
+                        .about("A simples CLI that informs you about your system")
+                        .arg(Arg::with_name("processes")
+                            .short("p")
+                            .long("processes")
+                            .value_name("PROCESS")
+                            .required(false)
+                            .empty_values(true))
+                        .arg(Arg::with_name("components")
+                            .short("c")
+                            .long("components")
+                            .value_name("COMPONENT")
+                            .required(false)
+                            .empty_values(true))
+                        .arg(Arg::with_name("help")
+                            .short("h")
+                            .long("help")
+                            .takes_value(false))
+                        .arg(Arg::with_name("v")
+                            .short("v")
+                            .takes_value(false))
+                        .get_matches();
 
-    let mut has_pattern = false;
-
-    for line in reader.lines() {
-        let line = line.unwrap();
-        if line.contains(&args.pattern) {
-            has_pattern = true;
-            println!("{}", line);
-        }
+    if matches.args.len() >= 3 && matches.is_present("help") && !matches.is_present("v") {
+        args::help_with_arg();
+    } else if matches.args.len() == 2 && matches.is_present("help") {
+        args::help_single_arg(matches);
+    } else if matches.args.len() == 1 && matches.is_present("help") {
+        args::help_all();
+    } else if matches.args.len() == 2 && matches.is_present("v") {
+        args::help_verbose(matches);
+    } else if matches.args.len() == 1 && !matches.is_present("help") {
+        args::call_arg_module(matches);
     }
-
-    if !has_pattern {
-        println!("No pattern {} found in {}", &args.pattern, &args.path.display().to_string());
-    }
+    
 }
